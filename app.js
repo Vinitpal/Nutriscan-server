596920145f1eb6028aefa2c4b01f01da89daf8e4
Vinitpal <vs361017@gmail.com>
2024-04-26 22:12:31 +0530

    cors error fix

--- app.js
+++ app.js
@@ -22,7 +22,12 @@ const connectDB = require('./db/connect');
 
 app.set('trust proxy', 1);
 app.use(helmet());
-app.use(cors());
+app.use(
+  cors({
+    credentials: true,
+    origin: true,
+  })
+);
 app.use(express.json());
 app.use(fileUpload({ useTempFiles: true }));
 app.use(morgan('dev'));
